34647121437b51c4e7de8e5fe4af0bb75bd43e6a
amit169 <amit.lalloo@standardbank.co.za>
2016-01-07 13:00:32 +0200

    Update development.js

--- config/env/development.js
+++ config/env/development.js
@@ -3,7 +3,14 @@
 
 // Set the 'development' environment configuration object
 module.exports = {
-	db: 'mongodb://72.17.0.2/mean-development',
+	
+	db: {
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-development',
+    options: {
+      user: '',
+      pass: ''
+    }
+	//db: 'mongodb://72.17.0.2/mean-development',
 	sessionSecret: 'developmentSessionSecret',
 	facebook: {
 		clientID: 'Facebook Application ID',
